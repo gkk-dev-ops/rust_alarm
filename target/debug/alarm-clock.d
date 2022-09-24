7b30e905b7b95dafe22262f5adc328ae2b7a28dd
kaczmarekgrzegorz11@gmail.com <gkk>
2022-09-24 21:30:26 +0200

    init

---
+++ target/debug/alarm-clock.d
@@ -0,0 +1,1 @@
+C:\Users\gkk\dev\rust\alarm-clock\target\debug\alarm-clock.exe: C:\Users\gkk\dev\rust\alarm-clock\src\main.rs
---
+++ target/debug/deps/alarm_clock.d
@@ -0,0 +1,5 @@
+C:\Users\gkk\dev\rust\alarm-clock\target\debug\deps\alarm_clock.exe: src\main.rs
+
+C:\Users\gkk\dev\rust\alarm-clock\target\debug\deps\alarm_clock.d: src\main.rs
+
+src\main.rs:
